a8b457f620df92e02619b079df86d4d4c980a27c
Mark Lin <marklin2289@gmail.com>
2021-02-23 14:07:51 -0800

    added CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
 import "./App.css";
 import Header from "./components/Header";
+
+// import Accessories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+// import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
+import Item from "./components/Item";
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* items => snapping */}
-      {/* items => snapping */}
-      {/* items => snapping */}
-      {/* items => snapping */}
+      <div className="app__itemsContainer">
+        <Item
+          title="Lowest Cost Solar Panels in American"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Model S"
+          desc="$69,420"
+          descLink=""
+          backgroundImg={ModelS}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model 3"
+          desc="$37,990"
+          descLink=""
+          backgroundImg={Model3}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model X"
+          desc="$79,990"
+          descLink=""
+          backgroundImg={ModelX}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model Y"
+          desc="$41,990"
+          descLink=""
+          backgroundImg={ModelY}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+      </div>
     </div>
   );
 }
